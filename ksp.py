3cdc6b53842f536bccb16c422f1716103541716f
Tom Hargreaves <hex@freezone.co.uk>
2015-08-22 22:22:29 +0100

    map etc.

--- ksp.py
+++ ksp.py
@@ -1,5 +1,6 @@
 import os, pygame, math
 from pygame.locals import *
+import pygame.gfxdraw
 
 class planet:
     r = 2000
@@ -11,48 +12,87 @@ class game:
     landed = False
     zoom = 1
     running = True
+    paused = False
     screen = None
+    map = False
 
 class ship:
-    x = 0
+    x = 0 #planet.r
     y = planet.r + 100
     dx = 0
     dy = 0
-    phi = math.pi/2
+    phi = 0  # -math.pi*5.5/4
     dphi = 0
     maxthrust = .02
-    thrust = 0
+    thrust = 0.5
+    sas = False
 
 def loop():
     SKY = (0,128,255)
     GROUND = (0,128,0)
+    RED = (255,0,0)
+    BLACK = (0,0,0)
+    WHITE = (255,255,255)
 
     game.clock.tick(60)
 
-    # Sky and planet
-    pygame.draw.rect(game.screen, SKY, (0,0,game.width,game.height))
-    planetx = game.width/2 + ship.x*math.sin(ship.phi) + ship.y*math.cos(ship.phi)
-    planety = game.height/2 - ship.x*math.cos(ship.phi) + ship.y*math.sin(ship.phi)
-    pygame.draw.circle(game.screen,(0,255,0), (int(planetx),int(planety)), planet.r)
-    print str(planetx) + ", " + str(planety)
+    if game.map:
+        # map screen
+        pygame.draw.rect(game.screen, BLACK, (0,0,game.width,game.height))
+        scale = 0.05
+        pygame.draw.circle(game.screen,GROUND, (int(game.width/2),int(game.height/2)), int(planet.r*scale))
+        shipx=int(game.width/2+ship.x*scale)
+        shipy=int(game.height/2+ship.y*scale)
+        pygame.draw.circle(game.screen,WHITE, (shipx, shipy), 2)
+        pygame.draw.line(game.screen,WHITE, (shipx,shipy), (int(shipx+10*math.cos(ship.phi)), int(shipy+10*math.sin(ship.phi))))
+        pygame.draw.line(game.screen,RED, (shipx,shipy), (int(shipx+ship.dx*4), int(shipy+ship.dy*4)))
+    else:
+        # Sky and planet
+        pygame.draw.rect(game.screen, SKY, (0,0,game.width,game.height))
+        planetx = game.width/2 + ship.x*math.sin(ship.phi) + ship.y*math.cos(ship.phi)
+        planety = game.height/2 - ship.x*math.cos(ship.phi) + ship.y*math.sin(ship.phi)
+        pygame.draw.circle(game.screen,(0,255,0), (int(planetx),int(planety)), planet.r)
+        n = 16
+        for i in xrange(1,n):
+            pygame.gfxdraw.pie(game.screen, int(planetx), int(planety), int(planet.r*10), 360*i/n, 360*(i+1)/n, (RED if i%2 else BLACK))
+        # Spaceship
+        if not game.crashed:
+            game.screen.blit(ship.rocket,(game.width/2-9,game.height/2-40))
+        else:
+            text = game.crashfont.render("CRASHED", 0, (255,0,0))
+            textpos = text.get_rect()
+            textpos.centerx = game.screen.get_rect().centerx
+            textpos.centery = game.screen.get_rect().centery
+            game.screen.blit(text,textpos)
 
-    # Spaceship
-    if not game.crashed:
-        game.screen.blit(ship.rocket,(game.width/2-9,game.height/2-40))
-        game.screen.blit(game.altimeter,(game.width/2-65+16-3,5))
-
+    # calculations
+    if not game.crashed and not game.paused:
         ship.dx += ship.maxthrust * ship.thrust * math.cos(ship.phi)
         ship.dy += ship.maxthrust * ship.thrust * math.sin(ship.phi)
+        print "thrust ", ship.maxthrust * ship.thrust * math.cos(ship.phi), ship.maxthrust * ship.thrust * math.sin(ship.phi)
+
         dr = math.sqrt(ship.dx*ship.dx+ship.dy*ship.dy)
         r = math.sqrt(ship.x*ship.x+ship.y*ship.y)
         theta = math.atan2(ship.y,ship.x)
+        
+        # altimeter
+        game.screen.blit(game.altimeter,(game.width/2-65+16-3,5))
+        text = game.font.render(str(int(r-planet.r)), 0, (20,20,20))
+        textpos = text.get_rect()
+        print textpos
+        textpos.centerx = game.screen.get_rect().centerx
+        textpos.centery += 14
+        game.screen.blit(text,textpos)
 
         ship.phi+=ship.dphi
         ship.x += ship.dx
         ship.y += ship.dy
+        if ship.sas:
+            if ship.dphi:
+                ship.dphi += -0.001 if ship.dphi>0 else 0.001
 
         landed = False
-        if (abs(r-planet.r)<=-dr and abs(dr) < 0.5 and 1):
+        if (abs(r-planet.r)<=-dr and abs(dr) < 0.5):
             ship.dx = ship.dy = 0
             landed = True
         elif (r < planet.r):
@@ -62,19 +102,10 @@ def loop():
             g = 0.01
             ship.dx -= g * math.cos(theta)
             ship.dy -= g * math.sin(theta)
-        text = game.font.render(str(int(r-planet.r)), 0, (20,20,20))
-        textpos = text.get_rect()
-        print textpos
-        textpos.centerx = game.screen.get_rect().centerx
-        textpos.centery += 14
-        game.screen.blit(text,textpos)
+            print "grav ", -g * math.cos(theta), -g * math.sin(theta)
         
-    else:
-        text = game.crashfont.render("CRASHED", 0, (255,0,0))
-        textpos = text.get_rect()
-        textpos.centerx = game.screen.get_rect().centerx
-        textpos.centery = game.screen.get_rect().centery
-        game.screen.blit(text,textpos)
+        print "theta=",theta
+        print ship.x, ship.dx, ship.y, ship.dy, r, ship.phi
     
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -92,15 +123,22 @@ def loop():
             elif event.key == ord('s'):
                 ship.thrust -= 1./16
             elif event.key == ord('a'):
-                ship.dphi += .001
-            elif event.key == ord('d'):
                 ship.dphi -= .001
+            elif event.key == ord('d'):
+                ship.dphi += .001
+        elif event.type == KEYUP:
+            if event.key == ord(' '):
+                game.paused = not game.paused
+            if event.key == ord('m'):
+                game.map = not game.map
+            if event.key == ord('t'):
+                ship.sas = not ship.sas
+                print "sas enabled" if ship.sas else "sas disabled"
 
     if ship.thrust < 0:
         ship.thrust = 0
     elif ship.thrust > 1:
         ship.thrust = 1
-    print ship.thrust
 
 def main():
     pygame.init()
