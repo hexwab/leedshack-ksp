6ff3876bd0f98451828bb28bd116179e95a354cc
davedude0 <harrymerckel@yahoo.co.uk>
2015-08-22 18:53:52 +0100

    Physics!

--- ksp.py
+++ ksp.py
@@ -5,8 +5,8 @@ class planet:
     r = 2000
 
 class game:
-    width = 320
-    height = 256
+    width = 320*2
+    height = 256*2
     crashed = False
     landed = False
     zoom = 1
@@ -15,7 +15,7 @@ class game:
 
 class ship:
     x = 0
-    y = planet.r
+    y = planet.r + 100
     dx = 0
     dy = 0
     phi = math.pi/2
@@ -31,12 +31,41 @@ def loop():
 
     # Sky and planet
     pygame.draw.rect(game.screen, SKY, (0,0,game.width,game.height))
-    planetx = 160 + ship.x*math.sin(ship.phi) + ship.y*math.cos(ship.phi)
-    planety = 128 - ship.x*math.cos(ship.phi) + ship.y*math.sin(ship.phi)
+    planetx = game.width/2 + ship.x*math.sin(ship.phi) + ship.y*math.cos(ship.phi)
+    planety = game.height/2 - ship.x*math.cos(ship.phi) + ship.y*math.sin(ship.phi)
     pygame.draw.circle(game.screen,(0,255,0), (int(planetx),int(planety)), planet.r)
 
     # Spaceship
-    game.screen.blit(ship.rocket,(160-9,128-40))
+    if not game.crashed:
+        game.screen.blit(ship.rocket,(game.width/2-9,game.height/2-40))
+
+        ship.dx += ship.maxthrust * ship.thrust * math.cos(ship.phi)
+        ship.dy += ship.maxthrust * ship.thrust * math.sin(ship.phi)
+        dr = math.sqrt(ship.dx*ship.dx+ship.dy*ship.dy)
+        r = math.sqrt(ship.x*ship.x+ship.y*ship.y)
+        theta = math.atan2(ship.y,ship.x)
+
+        ship.phi+=ship.dphi
+        ship.x += ship.dx
+        ship.y += ship.dy
+
+        landed = False
+        if (abs(r-planet.r)<=-dr and abs(dr) < 0.5 and 1):
+            ship.dx = ship.dy = 0
+            landed = True
+        elif (r < planet.r):
+            print("crashed")
+            game.crashed = True
+        else:
+            g = 0.01
+            ship.dx -= g * math.cos(theta)
+            ship.dy -= g * math.sin(theta)
+    else:
+        text = game.font.render("CRASHED", 0, (255,0,0))
+        textpos = text.get_rect()
+        textpos.centerx = game.screen.get_rect().centerx
+        textpos.centery = game.screen.get_rect().centery
+        game.screen.blit(text,textpos)
     
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -45,6 +74,7 @@ def loop():
         elif event.type == KEYDOWN:
             if event.key == ord('q'):
                 game.running = False
+    
 
 def main():
     pygame.init()
@@ -52,6 +82,7 @@ def main():
     game.screen = pygame.display.set_mode((game.width,game.height))
     game.clock = pygame.time.Clock()
     ship.rocket = pygame.image.load("images/real stuff/FULL ROKET.png")
+    game.font = pygame.font.Font(None, 64)
     while game.running:
         loop()
         pygame.display.flip()
