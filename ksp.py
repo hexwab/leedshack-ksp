39e29b46eb38c2b6f733d4169cbb895aa337eaad
Tom Hargreaves <hex@freezone.co.uk>
2015-08-22 18:09:01 +0100

    ...

--- ksp.py
+++ ksp.py
@@ -1,51 +1,56 @@
 import os, pygame, math
 from pygame.locals import *
 
-planet = { "r": 2000 }
-ship = { "x": 0, "y": planet["r"], "dx": 0, "dy": 0, "phi": math.pi/2, "dphi": 0, "maxthrust": .02, "thrust": 0 }
-crashed = False
-landed = False
-zoom=1
+class planet:
+    r = 2000
 
+class game:
+    width = 320
+    height = 256
+    crashed = False
+    landed = False
+    zoom=1
+    running = True
+    screen = None
 
-def main():
-    """this function is called when the program starts.
-       it initializes everything it needs, then runs in
-       a loop until the function returns."""
-#Initialize Everything
-    pygame.init()
-    screen = pygame.display.set_mode((640,480))
-    pygame.display.set_caption('Leedshack-KSP')
-    pygame.mouse.set_visible(0)
+class ship:
+    x = 0
+    y = planet.r
+    dx = 0
+    dy = 0
+    phi = math.pi/2
+    dphi = 0
+    maxthrust = .02
+    thrust = 0
 
-    pygame.display.flip()
-
-#Prepare Game Objects
-    clock = pygame.time.Clock()
-
+def loop():
     SKY = (0,128,255)
     GROUND = (0,128,0)
 
-#Main Loop
-    while 1:
-        clock.tick(60)
-        pygame.draw.rect(screen, SKY, (0,0,640,480))
-        planetx = 160 + ship["x"]*math.sin(ship["phi"]) + ship["y"]*math.cos(ship["phi"])
-        planety = 128 - ship["x"]*math.cos(ship["phi"]) + ship["y"]*math.sin(ship["phi"])
-        pygame.draw.circle(screen,(0,255,0), (int(planetx),int(planety)), planet["r"])
-        
+    game.clock.tick(60)
+    pygame.draw.rect(game.screen, SKY, (0,0,game.width,game.height))
+    planetx = 160 + ship.x*math.sin(ship.phi) + ship.y*math.cos(ship.phi)
+    planety = 128 - ship.x*math.cos(ship.phi) + ship.y*math.sin(ship.phi)
+    pygame.draw.circle(game.screen,(0,255,0), (int(planetx),int(planety)), planet.r)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            game.running = False
+            return
+        elif event.type == KEYDOWN:
+            if event.key == ord('q'):
+                game.running = False
 
-    #Handle Input Events
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                return
-            elif event.type == KEYDOWN:
-                if event.key == ord('q'):
-                    return
-
+def main():
+    pygame.init()
+    pygame.display.set_caption('Leedshack-KSP')
+    pygame.mouse.set_visible(0)
+    game.screen = pygame.display.set_mode((game.width,game.height))
+    game.clock = pygame.time.Clock()
+    while game.running:
+        loop()
         pygame.display.flip()
 
-
+    pygame.quit()
 
 #this calls the 'main' function when this script is executed
 if __name__ == '__main__': main()
